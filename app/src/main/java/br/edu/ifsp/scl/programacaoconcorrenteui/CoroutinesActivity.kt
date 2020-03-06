e5614feed4a42512c9ce7e4fe60f582314bb3878
Victor Bueno <victor.bueno@ringa.com.br>
2020-03-06 14:18:22 -0300

    Add GlobalScope coroutine for load images delay

--- app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
@@ -2,11 +2,29 @@ package br.edu.ifsp.scl.programacaoconcorrenteui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.scl.programacaoconcorrenteui.ProgramacaoConcorrenteActivity.Constantes.CARREGA_IMAGENS_DELAY
+import kotlinx.android.synthetic.main.activity_programacao_concorrente.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 
-class CoroutinesActivity : AppCompatActivity() {
+class CoroutinesActivity : ProgramacaoConcorrenteActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coroutines)
+//        setContentView(R.layout.activity_coroutines)
+        supportActionBar?.title = "Coroutines"
     }
+
+    override fun run() {
+        GlobalScope.launch {
+            sleep(CARREGA_IMAGENS_DELAY)
+
+            runOnUiThread {
+                altaIv.setImageResource(R.drawable.android_preto)
+                baixaIv.setImageResource(R.drawable.android_verde)
+            }
+        }
+    }
+
 }
